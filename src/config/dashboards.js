04dbf5e5215ad5191f8d2a71c20f7bcb5270a092
Md Nazmul Hasan <naha.14gm@gmail.com>
2026-09-24 12:32:33 +0600

    Update dashboards.js
    Sweater Dashboard has been added

--- src/config/dashboards.js
+++ src/config/dashboards.js
@@ -9,31 +9,17 @@
 
 export const dashboards = [
   {
-    slug: 'garments-combined',
-    title: 'Garments Combined',
-    description: 'Operational Summary',
+    slug: 'garments',
+    title: 'Garments Combined Dashboard',
+    description: 'Combined Garments Operational Summary',
     embedUrl:
       'https://docs.google.com/spreadsheets/d/e/2PACX-1vQkeOwYgRsouLtZQWEzfQun2J8dbbdoQhaQrYbpb9HZuL02PfgqmwXA0Zx-JKzYVLjmmuFhta9Pbd6i/pubhtml?gid=742606655&single=true&widget=true&headers=false'
   },
   {
-    slug: 'knitting',
-    title: 'Sweater-Knitting',
-    description: 'Target & Achievement Analysis',
+    slug: 'sweater',
+    title: 'Sweater Dashboard',
+    description: 'Sweater Operational Summary',
     embedUrl:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vTRHo2Mb9a4949DGRZHNiqRiHtpGfjNTfYeXoywSowekOgZCd1U8-cEOAE4Du7H0F4Kqwpsmm8DKVxm/pubhtml?widget=true&amp;headers=false'
-  },  
-  {
-    slug: 'linking',
-    title: 'Sweater-Linking',
-    description: 'Target & Achievement Analysis',
-    embedUrl:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vRl2QJYoMiFf1R0STOQlRrZekCDamX2ndNHxLE5eY5tKPJmrXKTdcpsXPWdOc1OW1fCBqzkwLOodBKI/pubhtml?gid=220845210&amp;single=true&amp;widget=true&amp;headers=false'
-  },
-  {
-    slug: 'finishing',
-    title: 'Sweater-Finishing',
-    description: 'Target & Achievement, Inspection, Shipment Schedule',
-    embedUrl:
-      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSlnmXoAsKeRGnQZcg6ri6H2bIdHSpmCQ6uPAZxSJdEEMGiy1G_EeSzbpoRnOawulNMcrM0HOscc-X7/pubhtml?gid=220845210&amp;single=true&amp;widget=true&amp;headers=false'
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vSBK-YsOV6CSggG9KCHTlFhHxOJ9CI0UZ-zW79ZiJUJkic2_r3Byx5tx1HDWISWAI4ZZwYbW4bNd3T0/pubhtml?gid=248940803&amp;single=true&amp;widget=true&amp;headers=false'
   }
 ]
